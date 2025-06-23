928ee80b6ec03760d0e8654744c52b28b614f402
zegasega <114316476+zegasega@users.noreply.github.com>
2025-06-23 16:44:14 +0300

    saas

--- routes/routes.js
+++ routes/routes.js
@@ -48,7 +48,7 @@ router.delete("/categories/:id", authMiddleware, roleMiddleware("admin"), (req, 
 // comments routes
 router.post("/comments", authMiddleware, (req, res) => commentController.createComment(req, res));
 router.get("/comments/post/:postId", authMiddleware, (req, res) => commentController.getCommentsByPost(req, res));
-router.delete("/comments/:commentId", authMiddleware, (req, res) => commentController.deleteComment(req, res));
+router.delete("/comments/delete/:commentId", authMiddleware, (req, res) => commentController.deleteComment(req, res));
 router.put("/comments/:id", authMiddleware, (req, res) => commentController.updateComment(req, res));
 
 router.get("/comments", authMiddleware, (req, res) => commentController.getAllComments(req, res));
